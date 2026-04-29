1cf0455799a2550c3eeb6fb50e364bbfdceb5acd
austinlin75104-creator <austinlin75104@gmail.com>
2026-04-29 15:41:15 +0800

    IMAPSource 實作連線與 SEARCH SINCE

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/austinjan/mail-agent
 
 go 1.26.1
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/emersion/go-imap/v2 v2.0.0-beta.8 // indirect
+	github.com/emersion/go-message v0.18.2 // indirect
+	github.com/emersion/go-sasl v0.0.0-20241020182733-b788ff22d5a6 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/source/imap.go
@@ -0,0 +1,71 @@
+package source
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	"github.com/emersion/go-imap/v2"
+	"github.com/emersion/go-imap/v2/imapclient"
+
+	"github.com/austinjan/mail-agent/internal/mail"
+)
+
+type IMAPConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+}
+
+type IMAPSource struct {
+	cfg IMAPConfig
+}
+
+func NewIMAPSource(cfg IMAPConfig) *IMAPSource {
+	return &IMAPSource{cfg: cfg}
+}
+
+// compile-time assertion: IMAPSource must satisfy Source.
+var _ Source = (*IMAPSource)(nil)
+
+func (s *IMAPSource) Fetch(folder string, since time.Time) ([]mail.Mail, uint32, error) {
+	c, err := s.dial()
+	if err != nil {
+		return nil, 0, fmt.Errorf("imap dial: %w", err)
+	}
+	defer c.Close()
+
+	if err := c.Login(s.cfg.User, s.cfg.Password).Wait(); err != nil {
+		return nil, 0, fmt.Errorf("imap login: %w", err)
+	}
+
+	selectData, err := c.Select(folder, &imap.SelectOptions{ReadOnly: true}).Wait()
+	if err != nil {
+		return nil, 0, fmt.Errorf("imap select %q: %w", folder, err)
+	}
+	uidValidity := selectData.UIDValidity
+
+	criteria := &imap.SearchCriteria{
+		Since: since,
+	}
+	searchData, err := c.UIDSearch(criteria, nil).Wait()
+	if err != nil {
+		return nil, 0, fmt.Errorf("imap search: %w", err)
+	}
+	uids := searchData.AllUIDs()
+	if len(uids) == 0 {
+		return nil, uidValidity, nil
+	}
+
+	// Full fetch is implemented in Task 09. Return empty slice for now.
+	_ = uids
+	return nil, uidValidity, nil
+}
+
+func (s *IMAPSource) dial() (*imapclient.Client, error) {
+	addr := fmt.Sprintf("%s:%d", s.cfg.Host, s.cfg.Port)
+	return imapclient.DialTLS(addr, &imapclient.Options{
+		TLSConfig: &tls.Config{ServerName: s.cfg.Host},
+	})
+}
